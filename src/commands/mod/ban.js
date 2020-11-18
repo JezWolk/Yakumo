f23b5cea3a9ccc0172a86583d7328f52477bf52c
iColtz <max.hurst@hotmail.com>
2020-11-18 07:29:28 +0000

    commands: added ban command

---
+++ src/commands/mod/ban.js
@@ -0,0 +1,54 @@
+const { Argument, Command } = require('discord-akairo');
+
+class BanCommand extends Command {
+    constructor() {
+        super('ban', {
+            aliases: ['ban'],
+            description: {
+                content: 'Bans a member from the guild.',
+                usage: '<member> [reason]',
+                examples: ['@Jez naughty.'],
+            },
+            clientPermissions: ['BAN_MEMBERS', 'EMBED_LINKS'],
+            userPermissions: ['BAN_MEMBERS'],
+            category: 'mod',
+            args: [
+                {
+                    id: 'user',
+                    type: Argument.union('member', async (_, input) => {
+                        try {
+                            const user = await this.client.users.fetch(input);
+                            return user || null;
+                        } catch { } // eslint-disable-line no-empty, brace-style
+                    }),
+                    prompt: {
+                        start: message => `${message.author}, what member would you like to ban?`,
+                    },
+                },
+                {
+                    id: 'reason',
+                    match: 'rest',
+                    type: 'string',
+                    default: '',
+                },
+            ],
+        });
+    }
+
+    async exec(message, { user, reason }) {
+        const member = message.guild.member(user.id);
+        if (member && member.roles.highest.rawPosition >= message.member.roles.highest.rawPosition) {
+            return message.channel.send('You know you can\'t ban this member, so why bother trying.');
+        }
+        const banMessage = await message.channel.send(`Banning **${user.tag}**...`);
+        try {
+            message.guild.members.ban(user.id, { reason: reason });
+            banMessage.edit(`Sucessfully banned **${member.user.tag}**`);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = BanCommand;
