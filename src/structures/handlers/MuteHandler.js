0d72ef9fa21289792a4ed118b4b2abd1edfddef7
iColtz <max.hurst@hotmail.com>
2020-11-18 22:09:30 +0000

    MuteHandler: unmutting member when their time is expired

--- src/structures/handlers/MuteHandler.js
+++ src/structures/handlers/MuteHandler.js
@@ -1,6 +1,32 @@
 class MuteHandler {
     constructor(client) {
         this.client = client;
+
+        setInterval(async () => {
+            for (const guild of this.client.guilds.cache) {
+                const mutedArray = await this.check(guild[0]);
+                for (const muted of mutedArray) {
+                    if (Date.now() >= Number(muted.expiryDate)) {
+                        this.endMute(muted.userID, guild[1], muted);
+                    }
+                }
+            }
+        }, 2000);
+    }
+
+    async endMute(member, guild, doc) {
+        member = guild.member(doc.userID);
+        if (member) {
+            try {
+                const mutedRole = await guild.roles.fetch(this.client.settings.mutedRole);
+                if (mutedRole) member.roles.remove(mutedRole.id);
+            }
+            catch { } // eslint-disable-line no-empty, brace-style
+        }
+        try {
+            await doc.deleteOne();
+        }
+        catch { } // eslint-disable-line no-empty, brace-style
     }
 
     async addMute(member, guild, duration) {
