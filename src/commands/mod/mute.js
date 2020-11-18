7f880ffda749cfc586ed41c2db7dd78ab7925e10
iColtz <max.hurst@hotmail.com>
2020-11-18 19:48:52 +0000

    commands: started mute command

---
+++ src/commands/mod/mute.js
@@ -0,0 +1,55 @@
+const { Command } = require('discord-akairo');
+const ms = require('ms');
+
+class MuteCommand extends Command {
+    constructor() {
+        super('mute', {
+            aliases: ['mute'],
+            description: {
+                content: 'Mutes a member from the guild.',
+                usage: '<member> <time> [reason]',
+                examples: ['@Coltz 10m bad.'],
+            },
+            clientPermissions: ['MANAGE_ROLES', 'EMBED_LINKS'],
+            userPermissions: ['MANAGE_ROLES'],
+            category: 'mod',
+            args: [
+                {
+                    id: 'member',
+                    type: 'member',
+                    prompt: {
+                        start: message => `${message.author}, what member would you like to mute?`,
+                    },
+                },
+                {
+                    id: 'time',
+                    type: (_, input) => {
+                        if (!input) return null;
+                        const len = ms(input);
+                        if (len && len <= 1296000000) return len;
+                        return null;
+                    },
+                    prompt: {
+                        start: message => `${message.author}, how long would you like to mute this member?`,
+                    },
+                },
+            ],
+        });
+    }
+    // eslint-disable-next-line no-unused-vars
+    async exec(message, { member, time }) {
+        if (member.roles.highest.rawPosition >= message.member.roles.highest.rawPosition) {
+            return message.channel.send('You know you can\'t mute this member, so why bother trying.');
+        }
+        const muteMessage = await message.channel.send(`Muting **${member.user.tag}**...`);
+        try {
+            // TODO: Handle the mute
+            muteMessage.edit('Mute Handler Not Complete Yet!');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = MuteCommand;
