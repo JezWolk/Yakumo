32f273ef989795bfecea42692bc09c8650cf98a1
iColtz <max.hurst@hotmail.com>
2020-11-21 21:48:08 +0000

    CaseHandler: sendModLog function

--- src/structures/handlers/CasesHandler.js
+++ src/structures/handlers/CasesHandler.js
@@ -1,3 +1,7 @@
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+const { MODLOG_COLORS } = require('../../Constants.js');
+
 class CasesHandler {
     constructor(client) {
         this.client = client;
@@ -16,6 +20,22 @@ class CasesHandler {
         catch (error) {
             console.log(error);
         }
+    }
+
+    async sendModlog(guild, data) {
+        const { action, mod, target, reason } = data;
+        const modlogChannel = guild.channels.cache.get(this.client.settings.modlog);
+        const modlog = new MessageEmbed()
+            .setColor(MODLOG_COLORS[action])
+            .setAuthor(`${mod.tag} - (${mod.id})`, mod.displayAvatarURL({ format: 'png', dynamic: true }))
+            .setThumbnail(target.user.displayAvatarURL({ format: 'png', dynamic: true }))
+            .setTimestamp(Date.now())
+            .setDescription(stripIndents`
+                **Member:** ${target.user.tag} - (${target.id})
+                **Action:** ${this.client.YakumoUtil.capitalize(action)}
+                **Reason:** ${reason || 'Not Specified'}
+            `);
+        modlogChannel.send(modlog);
     }
 }
 
