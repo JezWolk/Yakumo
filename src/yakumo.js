a05bff2ce1f577148d7338c4f6035f84b55a95f7
iColtz <max.hurst@hotmail.com>
2020-11-26 17:44:18 +0000

    client: fixed syntax issue

--- src/yakumo.js
+++ src/yakumo.js
@@ -1,4 +1,4 @@
-  const YAML = require('yaml');
+const YAML = require('yaml');
 const config = YAML.parse(require('fs').readFileSync('config.yaml', 'utf-8'));
 const YakumoClient = require('./client/YakumoClient.js');
 const client = new YakumoClient(config);
