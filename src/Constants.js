a632cf1d6aba376832cc97495726534349f4197a
iColtz <max.hurst@hotmail.com>
2020-11-26 17:37:26 +0000

    REWRITE

--- src/Constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MODLOG_COLORS: {
-        BAN: 0xff0000,
-        KICK: 0xffaa00,
-        MUTE: 0xffee00,
-    },
-};
--- src/models/export/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mute: require('../mute.js'),
-    guild: require('../guild.js'),
-    case: require('../case.js'),
-};
