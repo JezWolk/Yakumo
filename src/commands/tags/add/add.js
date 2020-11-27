9450ab494570ea5613a03e6f9db0c710504c14ea
iColtz <max.hurst@hotmail.com>
2020-11-27 20:58:26 +0000

    commands: tag and add tag command

---
+++ src/commands/tags/add/add.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord-akairo');
+
+class AddTagCommand extends Command {
+	constructor() {
+		super('tag-add', {
+			category: 'config',
+			channel: 'guild',
+            editable: true,
+			args: [
+				{
+					id: 'name',
+					type: 'tagName',
+					prompt: {
+						start: message => `${message.author}, what should the tag be named?`,
+						retry: (message, provided) => `${message.author}, a tag named **${provided.phrase}** already exists.`,
+					},
+				},
+				{
+					id: 'content',
+					match: 'rest',
+					type: 'tagContent',
+					prompt: {
+						start: message => `${message.author}, what should the content of the tag be?`,
+					},
+				},
+			],
+		});
+	}
+
+	async exec(message, { name, content }) {
+        if (content && content.length >= 1950) {
+            return message.util.send('The tag content length can\'t be over 1950 characters.');
+        }
+
+        await new this.client.models.tags({
+            guild: message.guild.id,
+            user_id: message.author.id,
+            name: name,
+            content: content,
+        }).save();
+
+        message.util.send(`Successfully created the tag **${name}**.`);
+	}
+}
+
+module.exports = AddTagCommand;
---
+++ src/commands/tags/tag.js
@@ -0,0 +1,29 @@
+const { Command, Flag } = require('discord-akairo');
+
+class TagCommand extends Command {
+	constructor() {
+		super('tag', {
+			aliases: ['tag'],
+			description: {
+				content: 'ill do this later!',
+				usage: 'ill do this later!',
+			},
+			category: 'tags',
+			channel: 'guild',
+            editable: true,
+		});
+	}
+
+    *args() {
+        const method = yield {
+          type: [
+            ['tag-add', 'add'],
+          ],
+          otherwise: 'Check `?help tag` for more information.',
+        };
+      
+        return Flag.continue(method);
+    }
+}
+
+module.exports = TagCommand;
