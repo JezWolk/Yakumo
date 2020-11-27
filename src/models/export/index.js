bf1a465f779f638bb2798086d650fc65da901435
iColtz <max.hurst@hotmail.com>
2020-11-27 20:56:46 +0000

    models: tags model

--- src/models/export/index.js
+++ src/models/export/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     settings: require('../settings.js'),
     cases: require('../cases.js'),
+    tags: require('../tags.js'),
 };
---
+++ src/models/tags.js
@@ -0,0 +1,28 @@
+const { model, Schema } = require('mongoose');
+
+const tagsSchema = new Schema({
+    guild: {
+        type: String,
+        required: true,
+    },
+    user_id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    uses: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+});
+
+module.exports = model('tags', tagsSchema);
